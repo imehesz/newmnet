4cf635d8357ae52310d2b74aca7afbb6e79f8e59
Imre Mehesz <imremehesz@WKST-TDS16-LX.(none)>
2010-07-19 17:57:33 -0400

    starting with the menu system

--- themes/halcyon/views/layouts/main.php
+++ themes/halcyon/views/layouts/main.php
@@ -14,10 +14,16 @@
 	    <?php $this->widget('zii.widgets.CMenu',array(
 			'items'=>array(
 				array('label'=>'Home', 'url'=>array('/site/index')),
-				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
+				array('label'=>'Blog', 'url'=>array('/blog/index')),
+				array('label'=>'Stuff', 'url'=>array('/stuff/index')),
+				array('label'=>'Podcast', 'url'=>array('/podcast/index')),
+				array('label'=>'Linux', 'url'=>array('/linux/index')),
+				// array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
 				array('label'=>'Contact', 'url'=>array('/site/contact')),
+                /*                
 				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
 				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
+                */
 			),
 		)); ?>
            
