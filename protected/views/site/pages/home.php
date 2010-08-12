6289fee494da93f68eaab1ecfb4684a50c0f55f3
Imre Mehesz <imremehesz@WKST-TDS16-LX.(none)>
2010-08-12 18:01:50 -0400

    started the home page with logos and stuff

---
+++ protected/views/site/pages/home.php
@@ -0,0 +1,13 @@
+<div style="background-color:#fff;text-align:center;">
+    <div style="float:left;width:50%;">
+        <p>
+            <a href="http://yiiframework.com" target="_blank"><img src="<?php echo Yii::app()->request->baseurl; ?>/images/yiilogo.jpg" /></a>
+        </p>
+    </div>
+    <div style="float:left;width:50%;">
+        <p>
+            <a href="http://drupal.org" target="_blank"><img src="<?php echo Yii::app()->request->baseurl; ?>/images/drupal-logo.png" /></a>
+        </p>
+    </div>
+    <div style="clear:both;"></div>
+</div>
