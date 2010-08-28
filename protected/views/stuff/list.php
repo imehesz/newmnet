17c2db311680c1ccb8b6df7daf8b1d18b63c67f6
Imre Mehesz <imehesz@imehesz-laptop.(none)>
2010-08-28 01:06:33 -0400

    started cleaning up some ugly CSS, so angels won't lose their wings

--- protected/views/stuff/list.php
+++ protected/views/stuff/list.php
@@ -2,6 +2,7 @@
 <?php Yii::app()->clientScript->registerScriptFile(Yii::app()->baseUrl.'/js/jquery.tools.min.js'); ?>
 <?php Yii::app()->clientScript->registerCssFile(Yii::app()->request->baseUrl.'/css/standalone.css'); ?>
 <?php Yii::app()->clientScript->registerCssFile(Yii::app()->request->baseUrl.'/css/overlay-apple.css'); ?>
+<?php Yii::app()->clientScript->registerCssFile(Yii::app()->request->baseUrl.'/css/stuff.css'); ?>
 
 <?php
 $this->breadcrumbs=array(
@@ -19,23 +20,23 @@ $this->menu=array(
 
 <div id="triggers">
 <?php foreach ($stuffs as $data) : ?> 
-		<div class="stuff-wrapper" style="min-height:200px;max-weight:200px;overflow:hidden;width:330px;border: 1px solid #ddd;float:left;margin:0px 0px 5px 5px;">
-			<div class="stuff-image" style="float:left;font-weight:bolder;min-height:150px;min-width:200px;">
+		<div class="stuff-wrapper">
+			<div class="stuff-image">
 				<?php if( file_exists( MEHESZ_FILES_FOLDER . $data->image ) ) : ?>
 					<a href="javascript:void(0);"><img src="<?php echo Yii::app()->image->createUrl( '200x150', MEHESZ_FILES_FOLDER . $data->image ); ?>" rel="#photo_<?php echo $data->id; ?>" border="0" /></a>
 				<?php else: ?>
 					<img src="<?php echo Yii::app()->request->baseUrl?>/images/nophoto.jpg" />
 				<?php endif; ?>
 			</div>
-			<div class="stuff-description" style="float:right;margin:5px 5px 0px 0px;width:120px;">
-				<span style="font-weight:bolder;">
+			<div class="stuff-description">
+				<span class="stuff-title">
 					<?php if( $data->extlink ) : ?>
 						<a href="<?php echo $data->extlink; ?>" target="_blank"><?php echo $data->name; ?></a>
 					<?php else: ?>
 						<?php echo $data->name; ?>
 					<?php endif; ?>
 				</span>
-				<div style="font-size:10px;margin-top:5px;"><?php echo $data->description; ?></div>
+				<div class="stuff-description-body"><?php echo $data->description; ?></div>
 			</div>
 			<div style="clear:both;"></div>
 		</div>
